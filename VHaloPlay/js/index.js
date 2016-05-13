edadc940b659997ab30746400d2b9e0af6624ab3
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2016-05-12 19:24:56 -0700

    initial commit, listen for message and play the received youtube video id, the icons are stock ones that are available

---
+++ VHaloPlay/js/index.js
@@ -0,0 +1,124 @@
+﻿(function () {
+    "use strict";
+    var playSocket = {};
+    playSocket.listener = null; 
+    playSocket.serverSocket = null;
+    playSocket.serverReader = null;
+    playSocket.serviceNameAccept = "22122";
+    WinJS.UI.Pages.define("index.html", {
+        ready: function (element, options) {
+            element.querySelector("#cmdPlay").addEventListener("click", doPlay, false);
+            element.querySelector("#cmdBackwardMicro").addEventListener("click", doBackMicro, false);
+            element.querySelector("#cmdBackwardSmall").addEventListener("click", doBackSmall, false);
+            element.querySelector("#cmdBackwardLarge").addEventListener("click", doBackLarge, false);
+            element.querySelector("#cmdForwardMicro").addEventListener("click", doForwardMicro, false);
+            element.querySelector("#cmdForwardSmall").addEventListener("click", doForwardSmall, false);
+            element.querySelector("#cmdForwardLarge").addEventListener("click", doForwardLarge, false);
+            var player = document.getElementById('videoPlayer');
+            player.width = window.innerWidth;
+            player.height = window.innerHeight - appBar.clientHeight;
+            startListener();
+        }
+    });
+
+    function startListener() {
+        if (playSocket.listener) {
+            return;
+        }
+        var serviceName = playSocket.serviceNameAccept;
+        playSocket.listener = new Windows.Networking.Sockets.StreamSocketListener();
+        playSocket.listener.addEventListener("connectionreceived", onServerAccept);
+        playSocket.listener.bindServiceNameAsync(serviceName).done(function () {
+            //listening
+        }, onError);
+    }
+
+    function onServerAccept(eventArgument) {
+        playSocket.serverSocket = eventArgument.socket;
+        playSocket.serverReader = new Windows.Storage.Streams.DataReader(playSocket.serverSocket.inputStream);
+        startServerRead();
+    }
+
+    function startServerRead() {
+        playSocket.serverReader.loadAsync(4).done(function (sizeBytesRead) {
+            if (sizeBytesRead !== 4) {
+                return;
+            }
+
+            var count = playSocket.serverReader.readInt32();
+            return playSocket.serverReader.loadAsync(count).then(function (stringBytesRead) {
+                if (stringBytesRead !== count) {
+                    return;
+                }
+                var string = playSocket.serverReader.readString(count);
+                handleCommand(string);
+                startServerRead();
+            });
+        }, onError);
+    }
+
+    function onError(reason) {
+        //handle error
+    }
+
+    function handleCommand(msg) {
+        var cmds = msg.split(",");
+        if (0 < cmds.length) {
+            switch (cmds[0]) {
+                case "playVideo":
+                    document.getElementById('videoId').value = cmds[1];
+                    doPlay();
+                    break;
+            }
+        }
+    }
+
+    function doPlayAt() {
+        if (0 > VHaloPlay.Video.approxCurrentLocation) {
+            VHaloPlay.Video.approxCurrentLocation = 0;
+        }
+        var appBar = document.getElementById('appBar');
+        var player = document.getElementById('videoPlayer');
+        player.width = window.innerWidth;
+        player.height = window.innerHeight - appBar.clientHeight;
+        player.src = "http://www.youtube.com/embed/" + VHaloPlay.Video.videoId + "?autoplay=1&start=" + VHaloPlay.Video.approxCurrentLocation;
+    }
+
+    function doPlay() {
+        VHaloPlay.Video.videoId = document.getElementById('videoId').value;
+        VHaloPlay.Video.approxCurrentLocation = 0;
+        doPlayAt();
+    }
+
+    function doBackMicro() {
+        VHaloPlay.Video.approxCurrentLocation -= VHaloPlay.Video.microStep;
+        doPlayAt();
+    }
+
+    function doBackSmall() {
+        VHaloPlay.Video.approxCurrentLocation -= VHaloPlay.Video.smallStep;
+        doPlayAt();
+    }
+
+    function doBackLarge() {
+        VHaloPlay.Video.approxCurrentLocation -= VHaloPlay.Video.largeStep
+        doPlayAt();
+    }
+
+    function doForwardMicro() {
+        VHaloPlay.Video.approxCurrentLocation += VHaloPlay.Video.microStep;
+        doPlayAt();
+    }
+
+    function doForwardSmall() {
+        VHaloPlay.Video.approxCurrentLocation += VHaloPlay.Video.smallStep;
+        doPlayAt();
+    }
+
+    function doForwardLarge() {
+        VHaloPlay.Video.approxCurrentLocation += VHaloPlay.Video.largeStep;
+        doPlayAt();
+    }
+
+
+})();
--- VHaloPlay/js/main.js
+++ VHaloPlay/js/main.js
@@ -1,24 +1,30 @@
-﻿(function() {
-  'use strict';
-  var app = WinJS.Application;
-  var activation = Windows.ApplicationModel.Activation;
-  app.onactivated = function (args) {
-    if (args.detail.kind === activation.ActivationKind.launch) {
-      if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-        // TODO: This application has been newly launched. Initialize your application here.
-      } else {
-        // TODO: This application has been reactivated from suspension.
-        // Restore application state here.
-      }
-      args.setPromise(WinJS.UI.processAll().then(function() {
-        // TODO: Your code here.
-      }));
-    }
-  };
-  app.oncheckpoint = function (args) {
-    // TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
-    // You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
-    // If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
-  };
-  app.start();
+﻿(function () {
+    'use strict';
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+            args.setPromise(WinJS.UI.processAll().then(function completed() {
+            }));
+        }
+    };
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+        // You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+        // If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+    };
+    WinJS.Namespace.define("VHaloPlay.Video", {
+        videoId: "UkWd0azv3fQ",
+        microStep: 5,
+        smallStep: 30,
+        largeStep: 600,
+        approxCurrentLocation: 0
+    });
+    app.start();
 }());
